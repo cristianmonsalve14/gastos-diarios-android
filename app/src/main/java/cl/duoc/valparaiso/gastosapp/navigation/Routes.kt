72de97f87e16c1e4bb0c36e4b9a1cd4d1f7ea629
Valparaiso <valparaiso@duoc.acad>
2025-11-18 21:22:14 -0300

    Este es un commit de pruba

--- app/src/main/java/cl/duoc/valparaiso/gastosapp/navigation/Routes.kt
+++ app/src/main/java/cl/duoc/valparaiso/gastosapp/navigation/Routes.kt
@@ -1,4 +1,5 @@
 package cl.duoc.valparaiso.gastosapp.navigation
+//Este es un comentario de prueba
 sealed class Route(val path: String) {
     data object Root : Route("root")
     data object Dashboard : Route("dashboard")
